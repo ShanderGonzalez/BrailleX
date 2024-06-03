f940d7ba0b736283b39b5fc161f10d6d25eb0c0c
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2024-06-03 18:39:01 -0500

    agregacin de interfaz

---
+++ Version1/implementation/templates/js/limpiar.js
@@ -0,0 +1,17 @@
+var elementos = document.getElementsByTagName('input');
+var respuesta = document.querySelector('.respuesta');
+var limpiar = document.getElementById('limpiarBoton');
+
+limpiar.onclick = (e) => { 
+  e.preventDefault();
+  for (let i = 0; i < elementos.length; i++) {
+    elementos[i].value = '';          
+  }
+  if (respuesta) {
+    // Si 'respuesta' es un div, span, etc.
+    respuesta.innerHTML = '';
+    // Si 'respuesta' es un input o textarea, usa:
+    // respuesta.value = '';
+  }
+}
+
