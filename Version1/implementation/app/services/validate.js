6db873e7935496a264b130a85aa8a8d949d82ec8
Xavier <xavier.carpio@epn.edu.ec>
2024-06-05 02:05:28 -0500

    Reparacion errores de merge

--- Version1/implementation/app/services/validate.js
+++ Version1/implementation/app/services/validate.js
@@ -1,23 +1,24 @@
 class Validator {
-    validarCorreo(correo) {
-        const patron = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return patron.test(correo);
-    }
+  validarCorreo(correo) {
+    const patron = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return patron.test(correo);
+  }
 
-    validarURL(url) {
-        const patron = /^(https?:\/\/)?www\.[^\s/$.?#].[^\s]*$/;
-        return patron.test(url);
-    }
+  validarURL(url) {
+    const patron = /^(https?:\/\/)?www\.[^\s/$.?#].[^\s]*$/;
+    return patron.test(url);
+  }
 
-    validarEtiqueta(etiqueta) {
-        const patronEtiqueta = /^#[a-zA-Z0-9_]+(:\d+)?$/;
-        return patronEtiqueta.test(etiqueta);
-    }
+  validarEtiqueta(etiqueta) {
+    const patronEtiqueta = /^#[a-zA-Z0-9_]+(:\d+)?$/;
+    return patronEtiqueta.test(etiqueta);
+  }
 
-    esEspanol(texto) {
-        const patron = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ0-9.,;:¿?¡!"«»'<>()[\]{}\-—*+\\/&@#%^$€\s]+$/;
-        return patron.test(texto);
-    }
+  esEspanol(texto) {
+    const patron =
+      /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ0-9.,;:¿?¡!"«»'<>()[\]{}\-—*+\\/&@#%^$€\s]+$/;
+    return patron.test(texto);
+  }
 }
 
 export default Validator;
