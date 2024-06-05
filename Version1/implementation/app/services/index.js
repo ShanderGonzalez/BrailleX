bcfda4639bb6153442134eea3d1ec42016263ab0
ShanderGonzalez <kevin.gonzalez@gaheos.com>
2024-06-05 00:03:02 -0500

    Correccion Traduccion de mayusculas

--- Version1/implementation/app/services/index.js
+++ Version1/implementation/app/services/index.js
@@ -4,7 +4,7 @@ import Traductor from "./traductor.js";
 const validador = new Validator();
 const traductor = new Traductor();
 const textoEspanol =
-    "Hola que tal, me llamo Shander y nací en Ecuador en el año 2002.\nSoy un estudiante de la carrera de Ingeniería en Software en la Universidad de la EPN. Me gusta mucho la programación y la tecnología.";
+    "Aji come";
 if (validador.esEspanol(textoEspanol)) {
     const textoBraille = traductor.traducirEspanolABraille(textoEspanol);
     console.log(textoBraille);
