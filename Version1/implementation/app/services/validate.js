ddb03cf3ee4370a6a1e7fb9cfcdf869e4448b79e
ShanderGonzalez <kevin.gonzalez@gaheos.com>
2024-06-28 16:25:30 -0500

    Refactorizacion

--- Version1/implementation/app/services/validate.js
+++ Version1/implementation/app/services/validate.js
@@ -9,18 +9,19 @@ class Validator {
    * @param {string} correo - El correo electrónico a validar.
    * @returns {boolean} - `true` si el correo es válido, `false` en caso contrario.
    */
-  validarCorreo(correo) {
-    const patron = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return patron.test(correo);
+  isValidEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
   }
+
   /**
      * Valida si una cadena de texto es una URL válida.
      * @param {string} url - La URL a validar.
      * @returns {boolean} - `true` si la URL es válida, `false` en caso contrario.
      */
-  validarURL(url) {
-    const patron = /^(https?:\/\/)?www\.[^\s/$.?#].[^\s]*$/;
-    return patron.test(url);
+  isValidURL(url) {
+    const urlPattern = /^(https?:\/\/)?www\.[^\s/$.?#].[^\s]*$/;
+    return urlPattern.test(url);
   }
 
   /**
@@ -28,20 +29,56 @@ class Validator {
    * @param {string} etiqueta - La etiqueta a validar.
    * @returns {boolean} - `true` si la etiqueta es válida, `false` en caso contrario.
    */
-  validarEtiqueta(etiqueta) {
-    const patronEtiqueta = /^#[a-zA-Z0-9_]+(:\d+)?$/;
-    return patronEtiqueta.test(etiqueta);
+  isValidTag(tag) {
+    const tagPattern = /^#[a-zA-Z0-9_]+(:\d+)?$/;
+    return tagPattern.test(tag);
   }
 
   /**
-   * Valida si una cadena de texto contiene solo caracteres en español.
-   * @param {string} texto - El texto a validar.
-   * @returns {boolean} - `true` si el texto contiene solo caracteres en español, `false` en caso contrario.
+   * Determina si un carácter es una letra.
+   * @param {string} caracter - El carácter a verificar.
+   * @returns {boolean} - Verdadero si el carácter es una letra, falso en caso contrario.
    */
-  esEspanol(texto) {
-    const patron =
-      /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ0-9.,;:¿?¡!"«»'<>()[\]{}\-—*+\\/&@#%^$€\s]+$/;
-    return patron.test(texto);
+  isLetter(char) {
+    return /[a-zA-Z\u00C0-\u00FF]/.test(char);
+  }
+
+  /**
+   * Determina si un carácter es un número.
+   * @param {string} caracter - El carácter a verificar.
+   * @returns {boolean} - Verdadero si el carácter es un número, falso en caso contrario.
+   */
+  isNumber(char) {
+    return /\d/.test(char);
+  }
+
+  /**
+   * Determina si un carácter es un espacio.
+   * @param {string} caracter - El carácter a verificar.
+   * @returns {boolean} - Verdadero si el carácter es un espacio, falso en caso contrario.
+   */
+  isSpace(char) {
+    return /\s/.test(char);
+  }
+
+  /**
+   * Determina si una palabra está en mayúsculas.
+   * @param {string} palabra - La palabra a verificar.
+   * @returns {boolean} - Verdadero si la palabra está en mayúsculas, falso en caso contrario.
+   */
+  isUpperCase(word) {
+    return word === word.toUpperCase() && /[a-zA-Z]/.test(word);
+  }
+
+  /**
+   * Determina si un signo es un punto decimal.
+   * @param {string} signo - El signo a verificar.
+   * @param {string} palabra - La palabra que contiene el signo.
+   * @param {number} indice - El índice del signo en la palabra.
+   * @returns {boolean} - Verdadero si el signo es un punto decimal, falso en caso contrario.
+   */
+  isDecimalPoint(sign, word, index) {
+    return sign === "." && this.isNumber(word[index - 1]) && this.isNumber(word[index + 1]);
   }
 }
 
