a2485416dee719a2cd8f2826a19374a746f85ae0
Xavier <94008723+xaviercarpio13@users.noreply.github.com>
2024-08-16 12:33:53 -0500

    Arreglo a Desplegable Conocenos

--- Version3-Final/implementation/app/utilities/responsive.js
+++ Version3-Final/implementation/app/utilities/responsive.js
@@ -1,27 +1,27 @@
 //funcion para cambiar el texto en las paginas responsive
 function cambiarTextoResponsive() {
   let elemento = document.getElementById("conocenosBoton");
-    if (window.innerWidth <= 600 ) {
-      elemento.textContent = '☰';
-    } else {
-      elemento.textContent = '☰';
-    }
+  if (window.innerWidth <= 600) {
+    elemento.textContent = "☰";
+  } else {
+    elemento.textContent = "Conócenos";
   }
-document.addEventListener('DOMContentLoaded', cambiarTextoResponsive);
-  
-window.addEventListener('resize', cambiarTextoResponsive);
+}
+document.addEventListener("DOMContentLoaded", cambiarTextoResponsive);
+
+window.addEventListener("resize", cambiarTextoResponsive);
 
 //funcion para ajustar el sidebar en las paginas responsive
-const sidebar = document.querySelector('.sidebar');
+const sidebar = document.querySelector(".sidebar");
 const sidebarParent = sidebar.parentNode;
 const sidebarNextSibling = sidebar.nextSibling;
 const originalSidebarHTML = sidebar.innerHTML;
 
 function ajustarSidebar() {
-  const traduceElement = document.getElementById('traduce');
-  
+  const traduceElement = document.getElementById("traduce");
+
   if (window.innerWidth <= 600) {
-    traduceElement.insertAdjacentElement('afterend', sidebar);
+    traduceElement.insertAdjacentElement("afterend", sidebar);
   } else if (sidebarNextSibling) {
     sidebarParent.insertBefore(sidebar, sidebarNextSibling);
   } else {
@@ -29,5 +29,5 @@ function ajustarSidebar() {
   }
 }
 
-window.addEventListener('DOMContentLoaded', ajustarSidebar);
-window.addEventListener('resize', ajustarSidebar);
+window.addEventListener("DOMContentLoaded", ajustarSidebar);
+window.addEventListener("resize", ajustarSidebar);
